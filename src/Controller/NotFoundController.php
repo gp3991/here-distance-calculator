988a195fe468036cf91661639a2fe6346fcb777b
gp3991 <gp3991@gmail.com>
2021-10-12 00:41:06 +0200

    Add throws tag to NotFoundController:index

--- src/Controller/NotFoundController.php
+++ src/Controller/NotFoundController.php
@@ -2,11 +2,15 @@
 
 namespace Gp3991\HereDistanceCalculator\Controller;
 
+use Gp3991\HereDistanceCalculator\Exception\HttpException;
 use Gp3991\HereDistanceCalculator\Exception\NotFoundHttpException;
 use Gp3991\HereDistanceCalculator\Http\ResponseInterface;
 
 class NotFoundController extends AbstractController
 {
+    /**
+     * @throws HttpException
+     */
     public function index(): ResponseInterface
     {
         throw new NotFoundHttpException();
