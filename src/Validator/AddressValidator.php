154bcab7138d7cd9e0ae03bbb677a84b1eca2bdf
gp3991 <gp3991@gmail.com>
2021-10-13 00:34:43 +0200

    Update swagger.json

--- src/Validator/AddressValidator.php
+++ src/Validator/AddressValidator.php
@@ -8,6 +8,9 @@ use Gp3991\HereDistanceCalculator\Model\Address;
 
 class AddressValidator implements ValidatorInterface
 {
+    public const WRONG_LATITUDE_MESSAGE = 'Given latitude is not a valid coordinate.';
+    public const WRONG_LONGITUDE_MESSAGE = 'Given longitude is not a valid coordinate.';
+    
     /**
      * @throws AssertionFailedException
      */
@@ -20,6 +23,14 @@ class AddressValidator implements ValidatorInterface
         Assertion::notEmpty($object->label, 'Field label is required.');
         Assertion::notEmpty($object->lat, 'Field lat (latitude) is required.');
         Assertion::notEmpty($object->lon, 'Field lon (longitude) is required.');
+        
+        Assertion::maxLength($object->label, 250, 'Maximum size of label is 250 characters.');
+
+        Assertion::greaterOrEqualThan($object->lat, -90.0, self::WRONG_LATITUDE_MESSAGE);
+        Assertion::lessOrEqualThan($object->lat, 90.0, self::WRONG_LATITUDE_MESSAGE);
+
+        Assertion::greaterOrEqualThan($object->lon, -180.0, self::WRONG_LONGITUDE_MESSAGE);
+        Assertion::lessOrEqualThan($object->lon, 180.0, self::WRONG_LONGITUDE_MESSAGE);
     }
 
     public function supports(object $object): bool
