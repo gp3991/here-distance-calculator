6a23a3611cbdf256ab9f4b2cfe59813340d7e038
gp3991 <gp3991@gmail.com>
2021-10-12 22:14:22 +0200

    Move index.php to web directory

--- web/index.php
+++ web/index.php
@@ -2,9 +2,9 @@
 
 use Gp3991\HereDistanceCalculator\App;
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->safeLoad();
 
 $dotenv->required(['DB_FILE', 'HERE_API_KEY'])->notEmpty();
